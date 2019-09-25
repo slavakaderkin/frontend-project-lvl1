284c678cb8b36741d40f3b90adeab66b66fad90c
Slava Kaderkin <slavakaderkin@gmail.com>
2019-09-25 11:42:39 +0400

    errors fixing

--- src/brain-even.js
+++ src/brain-even.js
@@ -1,7 +1,13 @@
 import readlineSync from 'readline-sync';
 
 const getRandomInt = (max) => Math.floor(Math.random() * Math.floor(max));
-const isEven = (num) => num % 2 === 0 ? 'yes' : 'no';
+const isEven = (num) => {
+  if (num % 2 === 0) {
+    return 'yes';
+  }
+
+  return 'no';
+};
 
 export default () => {
   const userName = readlineSync.question('May I have your name? ');
@@ -12,7 +18,7 @@ export default () => {
     const currentNum = getRandomInt(50);
 
     console.log(`Question: ${currentNum}`);
-	const rightAnswer = isEven(currentNum);
+    const rightAnswer = isEven(currentNum);
     const userAnswer = readlineSync.question('Your answer: ');
 
     if (userAnswer !== rightAnswer) {
